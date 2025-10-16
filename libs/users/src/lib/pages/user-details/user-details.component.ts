876bc54316f7ffe497d0aa80d88b65a3e6776f83
Mweruu <117080563+Mweruu@users.noreply.github.com>
2025-10-16 10:07:32 +0300

    Update user-details.component.ts

--- libs/users/src/lib/pages/user-details/user-details.component.ts
+++ libs/users/src/lib/pages/user-details/user-details.component.ts
@@ -44,7 +44,7 @@ export class UserDetailsComponent implements OnInit{
       country:['', Validators.required],
       zip:['', Validators.required],
       password:['', Validators.required],
-      isAdmin:[false]
+      isAdmin:[true]
     })
     this.getUsers();
     this._getCountries();
